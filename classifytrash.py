b1ade6e5cdea82d5a1e085835624cf446d399c14
Nathaniel <nattyfelleke@gmail.com>
2022-06-17 06:29:57 +0100

    2

--- classifytrash.py
+++ classifytrash.py
@@ -157,7 +157,7 @@ def main():
 
 while not gpsActive(): #wait for gps to be active before starting the script
     print("waiting for gps")
-    time.sleep(5)
+    time.sleep(20)
     
 print("starting")   
 main()
